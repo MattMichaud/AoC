dc0a52e20c5385aef5b6ef009bfb83d15fc7d3c9
Matt Michaud <matthew.michaud@ramseysolutions.com>
2021-01-18 10:34:36 -0600

    2018 day 20 completed

--- 2018/2018_20.py
+++ 2018/2018_20.py
@@ -1,8 +1,12 @@
 import numpy as np
 import re
+from collections import deque
+import operator
 
 door_offsets = {'N':(0,-1), 'S':(0,1), 'E': (1,0), 'W':(-1,0)}
 room_offsets = {'N':(0,-2), 'S':(0,2), 'E': (2,0), 'W':(-2,0)}
+
+class NotFound(Exception): pass
 
 def tuple_add(a, b):
     return(tuple(map(sum, zip(a, b))))
@@ -13,6 +17,9 @@ class RoomMap():
         self.rooms = set([self.start_loc])
         self.doors = set()
         self.process_regex(expression)
+        self.room_graph = self.build_room_graph()
+        self.room_distances = {}
+        self.update_room_distances()
 
     def add_room(self, current_loc, direction):
         new_room_loc = tuple_add(current_loc, room_offsets[direction])
@@ -85,20 +92,56 @@ class RoomMap():
                     current_locs[index] = self.add_room(loc, next_character)
             char_index += 1
 
+    def build_room_graph(self):
+        room_graph = {}
+        for r in self.rooms:
+            room_graph[r] = []
+            for c in 'NSEW':
+                possible_door_loc = tuple_add(r, door_offsets[c])
+                if possible_door_loc in self.doors:
+                    room_loc = tuple_add(r, room_offsets[c])
+                    room_graph[r].append(room_loc)
+        return(room_graph)
 
+    def bfs_search(self, start, goal):
+        visited = {start: None}
+        queue = deque([start])
+        while queue:
+            node = queue.popleft()
+            if node == goal:
+                path = []
+                while node is not None:
+                    path.append(node)
+                    node = visited[node]
+                return path[::-1]
+            for neighbour in self.room_graph[node]:
+                if neighbour not in visited:
+                    visited[neighbour] = node
+                    queue.append(neighbour)
+        raise NotFound('No path from {} to {}'.format(start, goal))
 
-test1 = '^ENWWW(NEEE|SSE(EE|N))$'
-test2 = '^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$'
-test3 = '^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$'
-test4 = '^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$'
+    def update_room_distances(self):
+        room_index = 1
+        for r in self.rooms:
+            room_index += 1
+            shortest_path = self.bfs_search(self.start_loc, r)
+            distance = len(shortest_path) - 1
+            self.room_distances[r] = distance
+
+    def furthest_room(self):
+        furthest_room = max(self.room_distances.items(), key=operator.itemgetter(1))[0]
+        return(self.room_distances[furthest_room])
+
+    def distant_rooms(self, distance):
+        room_count = 0
+        for r in self.rooms:
+            if self.room_distances[r] >= distance:
+                room_count += 1
+        return(room_count)
 
 with open('2018_20_input.txt','r') as f:
-    actual = f.read()
+    room_regex = f.read()
 
-my_map = RoomMap(test2)
-my_map.display()
-
-# still left to do:
-# create "map"
-# do BFS for each room to origin
-# get length of longest BFS
+my_map = RoomMap(room_regex)
+print('Part 1 Answer:', my_map.furthest_room())
+print('Part 2 Answer:', my_map.distant_rooms(1000))
