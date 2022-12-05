b1771416335d760ccf50c3b6af4d0607f3d4751f
MattMichaud <matthew.michaud@daveramsey.com>
2022-12-05 10:10:53 -0600

    Day 5 refactor to handle full input

--- 2022/code/05.py
+++ 2022/code/05.py
@@ -4,9 +4,41 @@ sys.path.append(".")
 from utils import data_import
 
 
-def parse_moves(li):
+def parse_input(filename):
+    crate_lines = []
+    move_lines = []
+    with open(filename) as file:
+        data = file.read().splitlines()
+    for line in data:
+        if line == "":
+            continue
+        elif line[0] == "m":
+            move_lines.append(line)
+        elif line[1] == "1":
+            total_crates = int(
+                line.lstrip().rstrip().replace("   ", ",").split(",")[-1]
+            )
+        else:
+            crate_lines.append(line)
+    moves = parse_moves(move_lines)
+    crates = parse_crates(crate_lines, total_crates)
+    return moves, crates
+
+
+def parse_crates(crate_list, total):
+    crates = [[] for i in range(total + 1)]
+    crate_list.reverse()
+    for l in crate_list:
+        for i in range(total):
+            c = l[(i * 4) + 1]
+            if c != " ":
+                crates[i + 1].append(c)
+    return crates
+
+
+def parse_moves(move_list):
     res = []
-    for l in li:
+    for l in move_list:
         l = l.replace("move ", "").replace(" from ", ",").replace(" to ", ",")
         l = l.split(",")
         l = [eval(i) for i in l]
@@ -14,76 +46,38 @@ def parse_moves(li):
     return res
 
 
-def accomplish_moves(crates, moves):
+def accomplish_moves(crates, moves, keepOrder=False):
     for m in moves:
-        qty = m[0]
-        source = m[1]
-        dest = m[2]
+        qty, source, dest = m
+        temp_list = []
         for i in range(qty):
             crate = crates[source].pop()
-            crates[dest].append(crate)
+            temp_list.append(crate)
+        if keepOrder:
+            temp_list.reverse()
+        crates[dest] = crates[dest] + temp_list
     return crates
 
 
-def accomplish_moves_multiple(crates, moves):
-    for m in moves:
-        qty = m[0]
-        source = m[1]
-        dest = m[2]
-        moved = crates[source][-1 * qty :]
-        crates[source] = crates[source][: -1 * qty]
-        crates[dest] = crates[dest] + moved
-    return crates
+def get_top_letters(crates):
+    res = ""
+    for stack in crates[1:]:
+        res += stack[-1]
+    return res
 
 
 def part1(crates, moves):
     crates = accomplish_moves(crates, moves)
-    res = ""
-    for stack in crates[1:]:
-        res += stack[-1]
-    print("Part 1 Answer:", res)
+    print("Part 1 Answer:", get_top_letters(crates))
 
 
 def part2(crates, moves):
-    crates = accomplish_moves_multiple(crates, moves)
-    res = ""
-    for stack in crates[1:]:
-        res += stack[-1]
-    print("Part 2 Answer:", res)
+    crates = accomplish_moves(crates, moves, True)
+    print("Part 2 Answer:", get_top_letters(crates))
 
 
-# input_file = "2022/inputs/test.txt"
 input_file = "2022/inputs/05.txt"
-
-test_crates = [[], ["Z", "N"], ["M", "C", "D"], ["P"]]
-crates = [
-    [],
-    ["C", "Z", "N", "B", "M", "W", "Q", "V"],
-    ["H", "Z", "R", "W", "C", "B"],
-    ["F", "Q", "R", "J"],
-    ["Z", "S", "W", "H", "F", "N", "M", "T"],
-    ["G", "F", "W", "L", "N", "Q", "P"],
-    ["L", "P", "W"],
-    ["V", "B", "D", "R", "G", "C", "Q", "J"],
-    ["Z", "Q", "N", "B", "W"],
-    ["H", "L", "F", "C", "G", "T", "J"],
-]
-
-data = data_import(input_file)
-moves = parse_moves(data)
+moves, crates = parse_input(input_file)
 part1(crates, moves)
-
-test_crates = [[], ["Z", "N"], ["M", "C", "D"], ["P"]]
-crates = [
-    [],
-    ["C", "Z", "N", "B", "M", "W", "Q", "V"],
-    ["H", "Z", "R", "W", "C", "B"],
-    ["F", "Q", "R", "J"],
-    ["Z", "S", "W", "H", "F", "N", "M", "T"],
-    ["G", "F", "W", "L", "N", "Q", "P"],
-    ["L", "P", "W"],
-    ["V", "B", "D", "R", "G", "C", "Q", "J"],
-    ["Z", "Q", "N", "B", "W"],
-    ["H", "L", "F", "C", "G", "T", "J"],
-]
+moves, crates = parse_input(input_file)
 part2(crates, moves)
