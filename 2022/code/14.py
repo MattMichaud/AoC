3a657269570360b2a4bcb275ab71341e2dc1c1b9
MattMichaud <matthew.michaud@daveramsey.com>
2022-12-14 12:52:03 -0600

    2022 Day 14 refactor 1

--- 2022/code/14.py
+++ 2022/code/14.py
@@ -1,8 +1,4 @@
-def tuple_add(a, b):
-    return tuple(map(sum, zip(a, b)))
-
-
-def parse_input(filename, start_loc, with_floor=False, floor_width=20):
+def build_cave(filename, start_loc):
     input_lines = open(filename, "r").read().strip().split("\n")
     structures = []
     for line in input_lines:
@@ -18,79 +14,49 @@ def parse_input(filename, start_loc, with_floor=False, floor_width=20):
             for y in range(start_y, end_y + 1):
                 for x in range(start_x, end_x + 1):
                     rock_locs.add((x, y))
-    c = build_cave(rock_locs, start_loc, with_floor, floor_width)
-    c[start_loc] = "+"
-    return c
-
-
-def build_empty_cave(top_left, bottom_right):
-    coords = {}
-    min_x, min_y = top_left
-    max_x, max_y = bottom_right
-    for y in range(min_y, max_y + 1):
-        for x in range(min_x, max_x + 1):
-            coords[(x, y)] = "."
-    return coords
-
-
-def get_dims(c):
-    tl = (min(x for x, _ in c), min(y for _, y in c))
-    br = (max(x for x, _ in c), max(y for _, y in c))
-    return tl, br
-
-
-def build_cave(rocks, start, with_floor, floor_width):
-    all_locs = rocks
-    all_locs.add(start)
-    floor_left = -1 * (floor_width // 2)
-    floor_right = floor_width // 2
-    floor_depth = get_dims(rocks)[1][1] + 2
-    floor_locs = [
-        (start[0] + x, floor_depth) for x in range(floor_left, floor_right + 1)
-    ]
-    if with_floor:
-        all_locs = list(all_locs) + floor_locs
-    top_left, bottom_right = get_dims(all_locs)
-    temp_cave = build_empty_cave(top_left, bottom_right)
-    for r in rocks:
+    temp_cave = {start_loc: "+"}
+    for r in rock_locs:
         temp_cave[r] = "#"
-    if with_floor:
-        for loc in floor_locs:
-            temp_cave[loc] = "F"
     return temp_cave
 
 
-def display(c, display_vals=["#", "o", "+"]):
-    used_locs = [(x, y) for x, y in c.keys() if c[(x, y)] in display_vals]
-    top_left, bottom_right = get_dims(used_locs)
-    min_x, min_y = top_left
-    max_x, max_y = bottom_right
-    for y in range(min_y, max_y + 1):
-        row = [c.get((x, y), " ") for x in range(min_x, max_x + 1)]
-        print("".join(row))
-
-
-def drop_sand(cave, drop_loc):
-    at_rest = False
-    off_edge = False
+def drop_sand(c, drop_loc, floor=False, bottom=0):
+    if not floor:
+        bottom = max(y for x, y in c if c[(x, y)] == "#") + 1
+    done = False
+    hit_bottom = False
     current_sand_loc = drop_loc
-    movements = {"D": (0, 1), "DL": (-1, 1), "DR": (1, 1)}
-    while not at_rest and not off_edge:
+    sand_moved = False
+    while not done:
         sand_moved = False
         for direction in ["D", "DL", "DR"]:
+            offset = {"D": (0, 1), "DL": (-1, 1), "DR": (1, 1)}.get(direction)
             if not sand_moved:
-                check_loc = tuple_add(current_sand_loc, movements.get(direction))
-                check_val = cave.get(check_loc, "ABYSS")
-                if check_val == ".":
+                check_loc = (
+                    current_sand_loc[0] + offset[0],
+                    current_sand_loc[1] + offset[1],
+                )
+                if check_loc[1] >= bottom:
+                    done = True
+                    hit_bottom = True
+                    sand_moved = True
+                elif check_loc not in c.keys():
                     current_sand_loc = check_loc
                     sand_moved = True
-                elif check_val == "ABYSS":
-                    off_edge = True
-                    sand_moved = True
-        if not off_edge and not sand_moved:
-            at_rest = True
-            cave[current_sand_loc] = "o"
-    return off_edge
+        if not hit_bottom and not sand_moved:
+            done = True
+    if floor or not hit_bottom:
+        c[current_sand_loc] = "o"
+
+    return hit_bottom
+
+
+def display_cave(c, top_left, bottom_right):
+    for y in range(top_left[1], bottom_right[1] + 1):
+        row = "".join(
+            c.get((x, y), ".") for x in range(top_left[0], bottom_right[0] + 1)
+        )
+        print(row)
 
 
 test_input = "test.txt"
@@ -98,13 +64,13 @@ puzzle_input = "2022/inputs/14.txt"
 current_file = puzzle_input
 
 start_loc = (500, 0)
-part1_cave = parse_input(current_file, start_loc)
+part1_cave = build_cave(current_file, start_loc)
 while not drop_sand(part1_cave, start_loc):
     next
 print("Part 1:", len([v for v in part1_cave.values() if v == "o"]))
 
-floor_width = 500
-part2_cave = parse_input(current_file, start_loc, True, floor_width)
+part2_cave = build_cave(current_file, start_loc)
+floor_depth = max(y for x, y in part2_cave.keys()) + 2
 while part2_cave[start_loc] == "+":
-    drop_sand(part2_cave, start_loc)
+    drop_sand(part2_cave, start_loc, floor=True, bottom=floor_depth)
 print("Part 2:", len([v for v in part2_cave.values() if v == "o"]))
